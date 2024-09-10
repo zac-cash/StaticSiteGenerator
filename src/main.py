16dda7b96ee06fb7fd09ab7400ebbd32350a4713
zac-cash <Personal@zac.cash>
2024-09-09 22:05:11 -0500

    inital commit

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode
+print('Hello World')
+
+def main():
+    test = TextNode("Test","Bold","www.dev.com")
+    print(test)
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,14 @@
+class TextNode():
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+    
+    def __eq__(self, otherNode):
+        pass
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+        
+
